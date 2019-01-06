36cc84fe664298e080553f9375d68f4bd0d0996b
MJedr <jedrych.marcysia@gmail.com>
2019-01-06 23:52:18 +0100

    End of part VI megatutorial. <username> URL not working

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, flash, url_for, redirect
+from flask import Flask, render_template, flash, url_for, redirect, request
 from flask_bootstrap import Bootstrap
-from app.forms import LoginForm
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from datetime import datetime
+
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,19 +22,76 @@ def create_app(config_name):
     bootstrap.init_app(app)
 
     from app import models
+    from app.forms import LoginForm, RegistrationForm, EditProfileForm
+    from app.models import Artist, login
+    login.init_app(app)
+
+
     @app.route('/')
     @app.route('/index')
     def index():
-        user = {'name': 'Marcjanna'}
-        return render_template('base.html', user=user)
+        return render_template('base.html')
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
         form = LoginForm()
         if form.validate_on_submit():
-            flash('Login requested for user {}, remember_me={}'.format(
-                form.username.data, form.remember_me.data))
+            user = Artist.query.filter_by(username=form.username.data).first()
+            if user is None or not user.check_password(form.password.data):
+                flash('Invalid username or password')
+                return redirect(url_for('login'))
+            login_user(user, remember=form.remember_me.data)
             return redirect(url_for('index'))
         return render_template('login.html', title='Sign In', form=form)
 
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            user = Artist(username=form.username.data, email=form.email.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash('Congratulations, you are now a registered user!')
+            return redirect(url_for('login'))
+        return render_template('register.html', title='Register', form=form)
+
+    @app.route('/user/<username>')
+    @login_required
+    def user(username):
+        user = Artist.query.filter_by(username=username).first_or_404()
+        posts = [
+            {'author': user, 'body': 'Test post #1'},
+            {'author': user, 'body': 'Test post #2'}
+        ]
+        return render_template('user.html', user=user)
+
+    @app.before_request
+    def before_request():
+        if current_user.is_authenticated:
+            current_user.last_seen = datetime.utcnow()
+            db.session.commit()
+
+    @app.route('/edit_profile', methods=['GET', 'POST'])
+    @login_required
+    def edit_profile():
+        form = EditProfileForm()
+        if form.validate_on_submit():
+            current_user.username = form.username.data
+            current_user.aboutMe = form.aboutMe.data
+            db.session.commit()
+            flash('Your changes have been saved.')
+            return redirect(url_for('edit_profile'))
+        elif request.method == 'GET':
+            form.username.data = current_user.username
+            form.about_me.data = current_user.aboutMe
+        return render_template('edit_profile.html', title='Edit Profile',
+                               form=form)
+
     return app
+
+
--- app/models.py
+++ app/models.py
@@ -1,14 +1,29 @@
 from sqlalchemy.orm import backref
-from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 from flask_login import UserMixin
+from hashlib import md5
+from datetime import datetime
 
-class Artist(db.Model):
+class Artist(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    aboutMe=db.Column(db.String(300))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -22,3 +37,6 @@ class Group(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
+@login.user_loader
+def load_user(id):
+    return Artist.query.get(int(id))
