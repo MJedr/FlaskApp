6e151dc3f00909a2f8a234cd74f67cd3c1ee3268
MJedr <jedrych.marcysia@gmail.com>
2019-01-07 20:18:46 +0100

    End of part VII of tutorial. Flash and url <username> not working

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from datetime import datetime
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
+
 
 
 
@@ -79,7 +84,7 @@ def create_app(config_name):
     @app.route('/edit_profile', methods=['GET', 'POST'])
     @login_required
     def edit_profile():
-        form = EditProfileForm()
+        form = EditProfileForm(current_user.username)
         if form.validate_on_submit():
             current_user.username = form.username.data
             current_user.aboutMe = form.aboutMe.data
@@ -91,6 +96,42 @@ def create_app(config_name):
             form.about_me.data = current_user.aboutMe
         return render_template('edit_profile.html', title='Edit Profile',
                                form=form)
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        db.session.rollback()
+        return render_template('500.html'), 500
+
+    if not app.debug:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                               backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+            app.logger.setLevel(logging.INFO)
+            app.logger.info('Microblog startup')
 
     return app
 
